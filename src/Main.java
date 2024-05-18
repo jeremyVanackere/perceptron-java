d3bbe2ffb4a5bc4523b785140819fa7ef1ab81b9
jeremy <jeremy.vanackere@outlook.fr>
2024-05-18 17:28:07 +0200

    test neuron 2 layer

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Perceptron perceptron = new Perceptron(2, 0.1); // 2 entrées, 1 sortie, taux d'apprentissage = 0.1
+        Perceptron perceptron1 = new Perceptron(2, 0.1); // 2 entrées, 1 sortie, taux d'apprentissage = 0.1
+        Perceptron perceptron2 = new Perceptron(2, 0.1); // 2 entrées, 1 sortie, taux d'apprentissage = 0.1
 
         double[][] inputs = {
                 {0, 0},
@@ -15,10 +17,15 @@ public class Main {
         for (int i = 0; i < 10000; i++) { // 10 000 itérations d'apprentissage
             for (int j = 0; j < inputs.length; j++) {
                 perceptron.train(inputs[j], targetResults[j]);
-                System.out.println("predict " + i + " " + j + " : " + perceptron.predict(inputs[j]) + " " + Arrays.toString(perceptron.getWeights()));
+                perceptron1.train(inputs[j], targetResults[j]);
+                perceptron2.train(
+                        new double[]{perceptron.predict(inputs[j]), perceptron1.predict(inputs[j])},
+                        targetResults[j]
+                );
+                System.out.println("predict " + i + " " + j + " : " + perceptron2.predict(inputs[j]) + " " + Arrays.toString(perceptron2.getWeights()));
             }
         }
-        double[] inputsTest = {0, 1};
+        double[] inputsTest = {0, 0};
         double output = perceptron.predict(inputsTest);
         System.out.println("Sortie : " + output);
     }
